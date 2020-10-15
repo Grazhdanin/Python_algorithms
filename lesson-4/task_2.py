ab27537e68bd6368c1ff9a0d230ce7538da3dcc8
Grazhdanin <serega_17_94@mail.ru>
2020-10-16 00:10:27 +0300

    lesson_4

---
+++ lesson-4/task_2.py
@@ -0,0 +1,57 @@
+"""
+Задание 2.
+
+Приведен код, который формирует из введенного числа
+обратное по порядку входящих в него цифр.
+Задача решена через рекурсию
+
+Сделайте замеры времени выполнения кода с помощью модуля timeit
+
+Попробуйте оптимизировать код, чтобы снизить время выполнения
+Проведите повторные замеры
+
+Подсказка: примените мемоизацию
+
+Добавьте аналитику: что вы сделали и почему
+"""
+
+import timeit
+
+def recursive_reverse(number):
+    if number == 0:
+        return str(number % 10)
+    return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+
+number = 1000
+
+print(timeit.timeit("recursive_reverse(number)", setup="from __main__ import recursive_reverse, number"))
+
+
+def memoize(f):
+    cache = {}
+
+    def decorate(*args):
+
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+    return decorate
+
+
+@memoize
+def recursive_reverse(number):
+    if number == 0:
+        return str(number % 10)
+    return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+
+print(timeit.timeit("recursive_reverse(number)", setup="from __main__ import recursive_reverse, number"))
+
+
+"""
+Мемоизация позволяет избежать лишних вычислений, поэтому выполняется быстрее.
+"""
+
